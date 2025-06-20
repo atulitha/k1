6299df3b1764f9979b6bb97ea6a474aad403353a
ABHINAV <domainlookup01@gmail.com>
2025-06-20 15:52:33 +0530

    Update autoplay timeout in custom.js for improved user experience; comment out unused carousel code

--- js/custom.js
+++ js/custom.js
@@ -49,7 +49,7 @@
       dots: true,
       autoplay: true,
       autoplayHoverPause: true,
-      autoplayTimeout: 1000,
+      autoplayTimeout: 5000,
       nav: false,
       responsive: {
         0: {
@@ -77,7 +77,40 @@
     }
   });
 
+//   $(document).ready(function(){
 
+//     var owl_1 = $('#owl-1');
+//     var owl_2 = $('#owl-2');
+
+//     owl_1.owlCarousel({
+//       loop:true,
+//       margin:10,
+//       nav:false,
+//       items: 1,
+//       dots: false,
+//       navText: false,
+//       autoplay: true,
+      
+//     });
+//  owl_2.find(".item").click(function(){
+//     var slide_index = owl_2.find(".item").index(this);
+//     owl_1.trigger('to.owl.carousel',[slide_index,300]);
+//   });
+
+//     owl_2.owlCarousel({
+//       margin:50,
+//       nav: true,
+//       items: 3,
+//       dots: false,
+//       center: true,
+//       loop:true,
+//       navText: false,
+//       autoplay: true,
+//       center: true
+//     });
+    
+//   });
+ 
 
 $('.counter').counterUp({
   time: 2000
